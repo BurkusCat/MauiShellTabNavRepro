66f6828186b47ccda002977510b8f8832ebc4c81
Ronan Burke <burkuscat@gmail.com>
2023-08-30 21:29:34 +0100

    New Maui project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiTabShell
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
